bd38d44facec5aa7feb2ea0b880eec56a371642b
firegloves <lucorset@gmail.com>
2022-05-02 15:08:05 +0200

    ENG-3515 use networking.k8s.io/v1 api group instead of extensions/v1beta1 for the created ingresses

--- src/test/java/org/entando/kubernetes/compositeapp/controller/interprocesstests/CompositeAppControllerIntegratedTest.java
+++ src/test/java/org/entando/kubernetes/compositeapp/controller/interprocesstests/CompositeAppControllerIntegratedTest.java
@@ -18,12 +18,15 @@ package org.entando.kubernetes.compositeapp.controller.interprocesstests;
 
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import org.entando.kubernetes.client.DefaultSimpleK8SClient;
 import org.entando.kubernetes.client.EntandoOperatorTestConfig;
 import org.entando.kubernetes.client.EntandoOperatorTestConfig.TestTarget;
 import org.entando.kubernetes.client.integrationtesthelpers.TestFixturePreparation;
 import org.entando.kubernetes.compositeapp.controller.AbstractCompositeAppControllerTest;
 import org.entando.kubernetes.compositeapp.controller.EntandoCompositeAppController;
+import org.entando.kubernetes.controller.spi.common.EntandoOperatorSpiConfigProperty;
 import org.entando.kubernetes.controller.support.common.EntandoOperatorConfigProperty;
+import org.entando.kubernetes.model.EntandoCustomResource;
 import org.entando.kubernetes.model.compositeapp.EntandoCompositeApp;
 import org.entando.kubernetes.model.compositeapp.EntandoCompositeAppOperationFactory;
 import org.entando.kubernetes.model.externaldatabase.EntandoDatabaseService;
@@ -93,4 +96,43 @@ class CompositeAppControllerIntegratedTest extends AbstractCompositeAppControlle
                         .deleteAll(EntandoKeycloakServer.class).fromNamespace(NAMESPACE));
     }
 
+
+
+
+
+
+
+
+//    String ENTANDO_RESOURCE_ACTION = "entando.resource.action";
+//    String TEST_CONTROLLER_POD = "test-controller-pod";
+//
+//    void runControllerAgainstCustomResource(EntandoCustomResource entandoCustomResource, KubernetesClient client) {
+//        try {
+//            final var simpleK8SClient = new DefaultSimpleK8SClient(client);
+//
+//            ///attachKubernetesResource(entandoCustomResource.getKind(), entandoCustomResource);
+//            System.setProperty(ENTANDO_RESOURCE_ACTION, Action.ADDED.name());
+//            System.setProperty(EntandoOperatorSpiConfigProperty.ENTANDO_RESOURCE_NAME.getJvmSystemProperty(),
+//                    entandoCustomResource.getMetadata().getName());
+//            System.setProperty(EntandoOperatorSpiConfigProperty.ENTANDO_RESOURCE_NAMESPACE.getJvmSystemProperty(),
+//                    entandoCustomResource.getMetadata().getNamespace());
+//            System.setProperty(EntandoOperatorSpiConfigProperty.ENTANDO_RESOURCE_KIND.getJvmSystemProperty(),
+//                    entandoCustomResource.getKind());
+//            System.setProperty(EntandoOperatorSpiConfigProperty.ENTANDO_CONTROLLER_POD_NAME.getJvmSystemProperty(),
+//                    TEST_CONTROLLER_POD);
+//            simpleK8SClient.entandoResources().createOrPatchEntandoResource(entandoCustomResource);
+//            final SimpleKeycloakClient keycloakClient = new DefaultKeycloakClient();
+//            final var commandStream = new InProcessCommandStream(simpleK8SClient, keycloakClient);
+//
+//            Runnable controller = new EntandoCompositeAppController(client);
+//            controller.run();
+//        } catch (RuntimeException e) {
+//            Logger.getLogger(getClass().getName()).log(Level.WARNING, e, e::getMessage);
+//            throw e;
+//        }
+//    }
+//
+//    public EntandoCompositeAppController createController() {
+//        return new EntandoCompositeAppController(client);
+//    }
 }
